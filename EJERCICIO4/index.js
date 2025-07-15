e9df2dfe63296b97ab307b690a569520f39bcbe1
lidiagdcso <lidia.gallardodc@gmail.com>
2025-07-15 20:30:12 +0200

    ejercicio 4

--- EJERCICIO4/index.js
+++ EJERCICIO4/index.js
@@ -1,12 +1,29 @@
-// 1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el
-// evento click que ejecute un console log con la información del evento del click
+// 1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el evento click que ejecute un console log con la información del evento del click
 
+let button = document.createElement('button')
+button.id = 'btnToClick'
+button.textContent = 'Haz click aqui'
+document.body.appendChild(button)
+
+button.onclick = function (event) {
+  console.log(event)
+}
+
+button.addEventListener('click', function (event) {
+  console.log(event)
+})
 // 1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
-
+let inputFocus = document.querySelector('.focus')
+inputFocus.addEventListener('focus', function (ev) {
+  console.log(inputFocus.value)
+})
 // 1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
+let inputValue = document.querySelector('.value')
+inputValue.addEventListener('input', function (ev) {
+  console.log(inputValue.value)
+})
 
-// Basandote en el array siguiente, crea una lista ul > li dinámicamente en el html
-// que imprima cada uno de los albums.
+// Basandote en el array siguiente, crea una lista ul > li dinámicamente en el html que imprima cada uno de los albums.
 
 const albums = [
   'De Mysteriis Dom Sathanas',
@@ -15,3 +32,14 @@ const albums = [
   'Painkiller',
   'Iron Fist'
 ]
+
+let ul = document.createElement('ul')
+
+for (const album of albums) {
+  let li = document.createElement('li')
+  let textLi = document.createTextNode(album)
+  li.appendChild(textLi)
+  ul.appendChild(li)
+}
+
+document.body.appendChild(ul)
